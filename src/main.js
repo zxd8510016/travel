e55f189f7ad34b164450ad48d2d0c8e0b188fa52
mac <mac@macdeMacBook-Pro.local>
2019-11-21 17:25:01 +0800

    change

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper)
 
 fastClick.attach(document.body)
 
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="home">
         <HomeHeader/>
+        <HomeSwiper/>
+        text
     </div>
     
 </template>
 
 <script>
 import HomeHeader from '../components/home-header'
+import HomeSwiper from '../components/swiper'
 export default {
     components:{
-        HomeHeader
+        HomeHeader,
+        HomeSwiper
     }
 }
 </script>
